f16e3cd32b01f055081a3e553e489772cd9ff504
Killian Paenen <36510425+kpaenen@users.noreply.github.com>
2025-07-02 09:48:12 +0000

    update lnglat

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import * as maplibregl from "https://esm.sh/maplibre-gl";
 import { Protocol } from "https://esm.sh/pmtiles";
+
 const protocol = new Protocol();
 maplibregl.addProtocol("pmtiles", protocol.tile);
 
 const map = new maplibregl.Map({
     container: 'mijnkaart', // container id
     style: './assets/style.json', // style URL
-    center: [51.96857, 5.66509], // starting position [lng, lat]
+    center: [5.66509, 51.96857], // starting position [lng, lat]
     zoom: 13 // starting zoom
 });
